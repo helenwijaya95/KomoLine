47a248e518b6f8c127035eac489755684633f125
Edo Fanuel Siswanto <dragunovoid@gmail.com>
2016-03-12 14:32:24 +0000

    Revert "lusi cuman nambahin d project web form buat Register."
    This reverts commit 27d683c92f99f18219d618477021067a5966c0cc

--- KomoLine.Entities/Model.Designer.cs
+++ KomoLine.Entities/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Data\ITHB\Semester 6\Rekayasa Perangkat Lunak Lanjut\KomoLine\KomoLine\KomoLine.Entities\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\College\Semester 6\Advanced System Engineering\Project\KomoLine.Entities\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
