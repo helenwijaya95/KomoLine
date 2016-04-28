4c6d15d2acacfe0f9f8db11d0d375b19714cf8d4
dynarizkihabibah <dyna.rizkii@gmail.com>
2016-04-28 16:49:53 +0700

    Layout Menu List User

--- KomoLine.WebForm/Pages/Template.Master.designer.cs
+++ KomoLine.WebForm/Pages/Template.Master.designer.cs
@@ -67,6 +67,15 @@ namespace KomoLine.WebForm.Pages {
         protected global::System.Web.UI.WebControls.HyperLink LinkToLink;
         
         /// <summary>
+        /// HyperLink1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
+        
+        /// <summary>
         /// txtsearch control.
         /// </summary>
         /// <remarks>
